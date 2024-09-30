c0374ddef637589c607aa8a44b6fdb14fed4b151
shrifzain <162094463+shrifzain@users.noreply.github.com>
2024-09-30 18:28:56 +0300

    Update backend.tf

--- Eks/eks/backend.tf
+++ Eks/eks/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-full"       # Replace with your S3 bucket name
+    bucket         = "terraform-state-full"       #   your S3 bucket 
     key            = "terraform-eks/state.tfstate"  # Path to the state file within the bucket
     region         = "us-west-2"                # Replace with your AWS region
     dynamodb_table = "terraform-locks"          # Replace with your DynamoDB table name
